c9a91aa934d0b0a555f9367864e785af7406b476
thulanimbatha <thulanimbatha06@gmail.com>
2023-01-17 01:34:39 +0200

    version 1.6 - removed nav links when user is logged in

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class User(UserMixin, db.Model):
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register',methods=['GET','POST'])
@@ -58,7 +58,7 @@ def register():
         login_user(new_user)
 
         return redirect(url_for('secrets'))
-    return render_template("register.html")
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -80,14 +80,14 @@ def login():
         else:
             login_user(user)
             return redirect(url_for('secrets'))
-    return render_template("login.html")
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/secrets')
 @login_required
 def secrets():
     print(current_user.name)
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route('/logout')
